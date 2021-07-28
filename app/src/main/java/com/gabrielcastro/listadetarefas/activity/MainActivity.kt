74375087f259ad0514865a6dc44c1baaef712ec0
Gabriel Castro <gabrielcssg@gmail.com>
2021-07-28 15:25:19 -0300

    click events

--- app/src/main/java/com/gabrielcastro/listadetarefas/activity/MainActivity.kt
+++ app/src/main/java/com/gabrielcastro/listadetarefas/activity/MainActivity.kt
@@ -5,14 +5,19 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.gabrielcastro.listadetarefas.R
 import com.gabrielcastro.listadetarefas.adapter.TodoAdapter
 import com.gabrielcastro.listadetarefas.databinding.ActivityMainBinding
+import com.gabrielcastro.listadetarefas.helper.RecyclerItemClickListener
 import com.gabrielcastro.listadetarefas.model.Todo
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -30,6 +35,25 @@ class MainActivity : AppCompatActivity() {
 
         // config recycler
         recyclerView = findViewById(R.id.recyclerTodo)
+        recyclerView.addOnItemTouchListener(
+            RecyclerItemClickListener(
+                applicationContext,
+                recyclerView,
+                object : RecyclerItemClickListener.OnItemClickListener {
+                    override fun onItemClick(view: View?, position: Int) {
+                        Toast.makeText(applicationContext, listTodo[position].name, Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onLongItemClick(view: View?, position: Int) {
+                        Toast.makeText(applicationContext, "LongItemClick ${listTodo[position].name}" , Toast.LENGTH_LONG).show()
+                    }
+
+                    override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                        TODO("Not yet implemented")
+                    }
+                }
+            )
+        )
 
         binding.fab.setOnClickListener {
             val intent = Intent(applicationContext, NewTodoActivity::class.java)
@@ -68,7 +92,10 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                Snackbar.make(binding.root, "settings", Snackbar.LENGTH_SHORT).show()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
