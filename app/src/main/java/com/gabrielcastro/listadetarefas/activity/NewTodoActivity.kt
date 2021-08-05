d0688d49df284fc2ccdd60034230df0656b9ce14
Gabriel Castro <gabrielcssg@gmail.com>
2021-08-05 11:50:55 -0300

    adicionado action button na toolbar de nova task

--- app/src/main/java/com/gabrielcastro/listadetarefas/activity/NewTodoActivity.kt
+++ app/src/main/java/com/gabrielcastro/listadetarefas/activity/NewTodoActivity.kt
@@ -2,11 +2,32 @@ package com.gabrielcastro.listadetarefas.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.gabrielcastro.listadetarefas.R
+import com.google.android.material.snackbar.Snackbar
 
 class NewTodoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_todo)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_secondary, menu);
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_save -> {
+                Snackbar.make(
+                    this.findViewById(R.id.contentNewTodo),
+                    "O item foi salvo!",
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
